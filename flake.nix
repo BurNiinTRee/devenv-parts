25b422c8f0325117ecc6ab4e2e81eaed41b3b5aa
Lars Mühmel <lars@muehml.eu>
2023-01-31 13:56:41 +0100

    merge both files

--- flake.nix
+++ flake.nix
@@ -2,10 +2,49 @@
   inputs = {
     devenv.url = "github:cachix/devenv";
     pre-commit-hooks.follows = "devenv/pre-commit-hooks";
-    nixpkgs.follows = "devenv/nixpkgs";
   };
 
-  outputs = {self, ...}: {
-    flakeModules.default = import ./module.nix self;
+  outputs = {
+    pre-commit-hooks,
+    devenv,
+    ...
+  }: {
+    flakeModules.default = {inputs, ...}: {
+      perSystem = {
+        config,
+        pkgs,
+        lib,
+        ...
+      }: {
+        options.devenv = lib.mkOption {
+          type = lib.types.submoduleWith {
+            modules = [
+              (devenv.modules + /top-level.nix)
+              ({
+                config,
+                pkgs,
+                ...
+              }: {
+                packages = [
+                  (import (devenv + /src/devenv-devShell.nix) {inherit config pkgs;})
+                ];
+                devenv.warnOnNewVersion = false;
+                devenv.flakesIntegration = true;
+              })
+              {
+                _module.args.pkgs = pkgs;
+                _module.args.inputs =
+                  {inherit pre-commit-hooks;} // inputs;
+              }
+              {
+                _module.args = {inherit pre-commit-hooks;} // inputs;
+              }
+            ];
+          };
+          default = {};
+        };
+        config.devShells.default = config.devenv.shell;
+      };
+    };
   };
 }
