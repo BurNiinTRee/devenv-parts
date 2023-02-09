5d0d240913f8db7f9d6a9096e73e3d01d5fab637
Lars Mühmel <lars@muehml.eu>
2023-02-09 18:16:13 +0100

    pass inputs using specialArgs
    this allows for importing devenvModules from
    inputs

--- flake.nix
+++ flake.nix
@@ -19,34 +19,36 @@
       }: {
         options.devenv = lib.mkOption {
           type = lib.types.attrsOf (lib.types.submoduleWith {
+            # Users might want to import devenvModules
+            # from their own inputs but overwriting
+            # them from a module doesn't seem necessary
+            # hence they should be specialArgs
+            specialArgs = let
+              args = {inherit pre-commit-hooks;} // inputs;
+            in
+              args // {inputs = args;};
             modules = [
               (devenv.modules + /top-level.nix)
-              ({
-                config,
-                pkgs,
-                ...
-              }: {
-                packages = [
-                  (import (devenv + /src/devenv-devShell.nix) {inherit config pkgs;})
-                ];
-                devenv.warnOnNewVersion = false;
-                devenv.flakesIntegration = true;
-              })
-              {
-                _module.args.pkgs = pkgs;
-                _module.args.inputs =
-                  {inherit pre-commit-hooks;} // inputs;
-              }
-              {
-                _module.args = {inherit pre-commit-hooks;} // inputs;
-              }
-              ({...}: {
+              ({config, lib, ...}: {
                 options.enable = lib.mkOption {
                   type = lib.types.bool;
                   default = true;
                   description = ''
                     Whether to enable this  devShell.
                   '';
+                };
+                config = {
+                  # Users might want to overwrite the pkgs arg
+                  # But importing from nixpkgs doesn't seem useful,
+                  # they could always import from inputs.nixpkgs as well.
+                  # the lib argument doesn't come from the pkgs argument,
+                  # so no problems with needing lib for the import path
+                  _module.args.pkgs = lib.mkOptionDefault pkgs;
+                  packages = [
+                    (import (devenv + /src/devenv-devShell.nix) {inherit config pkgs;})
+                  ];
+                  devenv.warnOnNewVersion = false;
+                  devenv.flakesIntegration = true;
                 };
               })
             ];
